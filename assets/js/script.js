ab07d6f18f07670c13b58381116e9b1c3142d968
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-04-04 12:23:16 +0100

    added boilerplate code for server request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
+const functionForJSON = (responseObject) => responseObject.json();
+
+const runApplication = () => {
+  console.log("data downloaded");
+};
+
+const handleErrors = () => {
+  console.log("something went wrong");
+};
+
+const requestServerData = (url) => {
+  fetch(url).then(functionForJSON).then(runApplication).catch(handleErrors);
+};
+
 const onReady = () => {
-  console.log("I'm ready");
+  const myURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=birmingham&appid=524c8c0dbcbfa8a1202c6a2b9d272ee1";
+  requestServerData(myURL);
 };
 
 $("document").ready(onReady);
